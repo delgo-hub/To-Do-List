47f355424b09c203b566b433ca3a13b585518b1c
delgo123 <75376053+delgo123@users.noreply.github.com>
2022-04-29 21:07:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+//DOM VARIABLES
+const taskInput = document.querySelector(".task-input");
+const taskContainer = document.getElementById("tasks-container");
+const taskContainerSmaller = document.getElementById("task-container-smaller");
+const taskCounterEl = document.querySelector(".task-counter");
+const addBtn = document.querySelector(".add-task");
+const deleteAllBtn = document.querySelector(".delete-all-btn");
+
+//VARIABLES
+let counter = 0;
+
+//EVENTS
+addBtn.addEventListener("click", () => {
+    if (taskInput.value === "") return;
+
+    //CREATE HTML ELEMENTS
+    const taskLi = document.createElement("li");
+    const taskContent = document.createTextNode(taskInput.value);
+    const checkBox = document.createElement("input");
+    const deleteBtn = document.createElement("i");
+
+    //SET ATTRIBUTES
+    taskLi.setAttribute("class", "task-content");
+    checkBox.setAttribute("class", "check-box");
+    checkBox.setAttribute("type", "checkbox");
+    deleteBtn.setAttribute("class", "fa-solid fa-trash");
+
+    //APPEND ELEMENTS
+    taskLi.appendChild(checkBox);
+    taskLi.appendChild(taskContent);
+    taskLi.appendChild(deleteBtn);
+    taskContainerSmaller.appendChild(taskLi);
+
+    //RESET INPUT
+    taskInput.value = "";
+
+    //INCREMENT COUNTER
+    counter++;
+    taskCounterEl.textContent = `${counter} TASKS`;
+
+    //CHECKBOX EVENT
+    checkBox.addEventListener("click", () => {
+        taskLi.classList.toggle("checked");
+
+        if (taskLi.classList.contains("checked")) {
+            counter--;
+            taskCounterEl.textContent = `${counter} TASKS`;
+        } else {
+            counter++;
+            taskCounterEl.textContent = `${counter} TASKS`;
+        }
+    });
+
+    //DELETE BUTTON EVENT
+    deleteBtn.addEventListener("click", () => {
+        taskContainerSmaller.removeChild(taskLi);
+        counter--;
+        taskCounterEl.textContent = `${counter} TASKS`;
+    });
+});
+
+//DELETE ALL TASKS EVENT
+deleteAllBtn.addEventListener("click", () => {
+    taskContainerSmaller.replaceChildren("");
+
+    //RESET COUNTER
+    counter = 0;
+    taskCounterEl.textContent = `${counter} TASKS`;
+});
